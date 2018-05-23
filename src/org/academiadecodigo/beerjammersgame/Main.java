8d75f1b05409c7301c4d8ceef7be46a6f6eb222e
Code Cadet <codecadet@swartz.local>
2018-05-23 18:36:53 +0000

    Added resouses directory

--- src/org/academiadecodigo/beerjammersgame/Main.java
+++ src/org/academiadecodigo/beerjammersgame/Main.java
@@ -5,6 +5,7 @@ import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
+
         Menu menu = new Menu();
 
         Rectangle screen = new Rectangle(10, 10, 1500, 900);
--- src/org/academiadecodigo/beerjammersgame/menu/MenuMouseHandler.java
+++ src/org/academiadecodigo/beerjammersgame/menu/MenuMouseHandler.java
@@ -1,13 +1,15 @@
 package org.academiadecodigo.beerjammersgame.menu;
 
-import org.academiadecodigo.simplegraphics.graphics.*;
 import org.academiadecodigo.simplegraphics.mouse.*;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 
 public class MenuMouseHandler implements MouseHandler {
 
-    Rectangle r = new Rectangle(50, 50, 50, 50);
+    //Rectangle initMenu = new Rectangle(50, 50, 50, 50);
 
+    Picture initMenu = new Picture(10,10, "./BeerJammersMenuInit.png");
+    Picture selectMenu = new Picture(10,10, "./BeerJammersMenuInit.png");
 
     public MenuMouseHandler() {
 
@@ -15,17 +17,21 @@ public class MenuMouseHandler implements MouseHandler {
         m.addEventListener(MouseEventType.MOUSE_CLICKED);
         m.addEventListener(MouseEventType.MOUSE_MOVED);
 
-        r.setColor(Color.RED);
-        r.fill();
+        //initMenu.setColor(Color.RED);
+        initMenu.draw();
 
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         //System.out.println(e);
-        if(e.getX() >= r.getX() && e.getX() <= (r.getX() + r.getWidth()) && e.getY() >= (r.getY() + 25) && e.getY() <= (r.getY() + r.getHeight() + 25) && r.isFilled()){
-            System.out.println("You Delete Rectangle");
-            r.delete();
+        if(e.getX() >= 520 && e.getX() <= 980 && e.getY() >= (530) && e.getY() <= (700)){
+            System.out.println("You press Start Button");
+            selectMenu.draw();
+            initMenu.delete();
+        }
+        if(e.getX() >= 520 && e.getX() <= 980 && e.getY() >= (730) && e.getY() <= (890)){
+            System.out.println("You press Controls Menu");
         }
     }
 
